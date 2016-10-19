28b43c2e730cf4d5d31d00d4d6148115eddc3419
Jacob Allen Utley <jacob@halfinjest.com>
2016-10-19 08:56:19 -0500

    Another commit.

--- refer.php
+++ refer.php
@@ -1,7 +1,6 @@
 <?php
 
-date_default_timezone_set('America/Chicago');
-
+date_default_timezone_set("America/Chicago");
 $directory = "./";
 if (isset($_GET["path"])) {
 	$directory = $_GET["path"];
