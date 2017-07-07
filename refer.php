fd604d8c6359696568210b52ab1fdaf60b8ef620
Jacob Allen Utley <jacob@halfinjest.com>
2017-07-07 18:48:24 -0500

    Another commit.

--- refer.php
+++ refer.php
@@ -32,7 +32,7 @@ if ($length > 1) while ($i < $length - 1)
 {
 	$j += CONF_COLS;
 	echo "<tr>\n";
-	if ($i == 0) echo "<td>\n<a href=\"refer.php?path=".dirname($directory)."/\">\n<div class=\"item\">\n<p align=\"center\"><img height=\"50px\" src=\"octicons/file-symlink-directory.svg\"></img></p>\n<p align=\"center\">[Parent]</p>\n</div>\n</a>\n</td>\n";
+	if ($i == 0) echo "<td>\n<a href=\"refer.php?path=".dirname($directory)."/\">\n<div class=\"item\" onmouseout=\"mouseout()\" onmouseover=\"mouseover('../')\">\n<p align=\"center\"><img height=\"50px\" src=\"octicons/file-symlink-directory.svg\"></img></p>\n<p align=\"center\">[Parent]</p>\n</div>\n</a>\n</td>\n";
 	while ($i < $j - 1 && ++$i < $length)
 	{
 		if (is_dir($directory.$index[$i])) echo "<td>\n<a href=\"refer.php?path=".$directory.$index[$i]."/\">\n<div class=\"item\" onmouseout=\"mouseout()\" onmouseover=\"mouseover('".$index[$i]."/')\">\n<p align=\"center\"><img height=\"50px\" src=\"octicons/file-directory.svg\"></img></p>\n<p align=\"center\">".$index[$i]."</p>\n</div>\n</a>\n</td>\n";
@@ -99,7 +99,7 @@ if ($length > 1) while ($i < $length - 1)
 	if ($length < CONF_COLS) for ($i = $length; $i < CONF_COLS; $i++) echo "<td>\n</td>\n";
 	echo "</tr>\n";
 }
-else echo "<tr>\n<td>\n<a href=\"refer.php?path=".dirname($directory)."/\">\n<div class=\"item\">\n<p align=\"center\"><img height=\"50px\" src=\"octicons/file-symlink-directory.svg\"></img></p>\n<p align=\"center\">[Parent]</p>\n</div>\n</a>\n</td>\n<td>\n</td>\n<td>\n</td>\n<td>\n</td>\n</tr>\n";
+else echo "<tr>\n<td>\n<a href=\"refer.php?path=".dirname($directory)."/\">\n<div class=\"item\" onmouseout=\"mouseout()\" onmouseover=\"mouseover('../')\">\n<p align=\"center\"><img height=\"50px\" src=\"octicons/file-symlink-directory.svg\"></img></p>\n<p align=\"center\">[Parent]</p>\n</div>\n</a>\n</td>\n<td>\n</td>\n<td>\n</td>\n<td>\n</td>\n</tr>\n";
 ?>
 </table>
 </div>
