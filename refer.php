028e49c7ca833f33cbf57210515425822392aee6
Jacob Allen Utley <jacob@halfinjest.com>
2017-07-09 16:53:54 -0500

    Changed constants.

--- refer.php
+++ refer.php
@@ -2,7 +2,7 @@
 
 include("image.php");
 
-define("CONF_COLS", 4);
+define("CONF_COL", 4);
 
 if (isset($_GET["path"]) && file_exists($_GET["path"]) && substr($_GET["path"], 0, 2) == "./") $directory = $_GET["path"];
 else header("Location: ?path=./");
@@ -29,7 +29,7 @@ $index = array_slice(scandir($directory), 1);
 $length = count($index);
 if ($length > 1) while ($i < $length - 1)
 {
-	$j += CONF_COLS;
+	$j += CONF_COL;
 	echo "<tr>\n";
 	if ($i == 0) echo "<td>\n<a href=\"?path=".dirname($directory)."/\">\n<div class=\"item\" onmouseout=\"path('".$directory."')\" onmouseover=\"path('".dirname($directory)."/')\">\n<p align=\"center\"><img height=\"50px\" src=\"images/file-symlink-directory.svg\"></img></p>\n<p align=\"center\">[Parent]</p>\n</div>\n</a>\n</td>\n";
 	while ($i < $j - 1 && ++$i < $length)
@@ -39,7 +39,7 @@ if ($length > 1) while ($i < $length - 1)
 		if (is_dir($directory.$index[$i])) echo "<td>\n<a href=\"?path=".$directory.$index[$i]."/\">\n<div class=\"item\" onmouseout=\"path('".$directory."')\" onmouseover=\"path('".$directory.$index[$i]."/')\">\n<p align=\"center\"><img height=\"50px\" src=\"images/file-directory.svg\"></img></p>\n<p align=\"center\">".$name."</p>\n</div>\n</a>\n</td>\n";
 		else echo "<td>\n<a href=\"".$directory.$index[$i]."\">\n<div class=\"item\" onmouseout=\"path('".$directory."')\" onmouseover=\"path('".$directory.$index[$i]."')\">\n<p align=\"center\"><img height=\"50px\" src=\"images/".image(strtolower(strrchr($index[$i], ".")))."\"></img></p>\n<p align=\"center\">".$name."</p>\n</div>\n</a>\n</td>\n";
 	}
-	if ($length < CONF_COLS) for ($i = $length; $i < CONF_COLS; $i++) echo "<td>\n</td>\n";
+	if ($length < CONF_COL) for ($i = $length; $i < CONF_COL; $i++) echo "<td>\n</td>\n";
 	echo "</tr>\n";
 }
 else echo "<tr>\n<td>\n<a href=\"?path=".dirname($directory)."/\">\n<div class=\"item\" onmouseout=\"path('".$directory.$index[$i]."')\" onmouseover=\"path('".dirname($directory)."/')\">\n<p align=\"center\"><img height=\"50px\" src=\"images/file-symlink-directory.svg\"></img></p>\n<p align=\"center\">[Parent]</p>\n</div>\n</a>\n</td>\n<td>\n</td>\n<td>\n</td>\n<td>\n</td>\n</tr>\n";
