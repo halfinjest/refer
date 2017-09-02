bfc222f55bf9eeddf2091cd3ca790fa148e4ec35
Jacob Allen Utley <jacob@halfinjest.com>
2017-09-02 01:52:51 -0500

    Removed string safety.

--- refer.php
+++ refer.php
@@ -4,15 +4,15 @@ include("type.php");
 
 define("CONF_COL", 4);
 define("CONF_MAX", 20);
-
-function get_safe_path($path)
-{
-	return str_replace("//", "/", str_replace("..", "", $path));
-}
 	
 function get_item($directory, $id, $image, $index, $name, $path, $prefix)
 {
 	return "<td>\n<a href=\"".$prefix.$path."\">\n<div class=\"item\" id=\"".$id."\" onmouseout=\"hover(0, '".$id."', '".$directory."', '')\" onmouseover=\"hover(1, '".$id."', '".$path."', '".(is_dir($path) ? "directory" : type(strtolower(strrchr($index[$id], "."))))."')\">\n<p align=\"center\"><img height=\"50px\" src=\"images/".$image."\"></img></p>\n<p align=\"center\">".$name."</p>\n</div>\n</a>\n</td>\n";
+}
+
+function get_safe_path($path)
+{
+	return str_replace("//", "/", str_replace("..", "", $path));
 }
 
 if (!isset($_GET["path"]) || !file_exists($_GET["path"]) || !substr($_GET["path"], 0, 1) == "." || !substr($_GET["path"], 1, 2) == "/") header("Location: ?path=./");
@@ -53,19 +53,19 @@ if ($length > 1) while ($i < $length - 1)
 {
 	$j += CONF_COL;
 	printf("<tr>\n");
-	if ($i == 0) printf(get_item(htmlentities($directory), $i, "file-symlink-directory.svg", $index, "[Parent]", dirname($directory)."/", "?path="));
+	if ($i == 0) printf(get_item($directory, $i, "file-symlink-directory.svg", $index, "[Parent]", dirname($directory)."/", "?path="));
 	while ($i < $j - 1 && ++$i < $length)
 	{
 		$path = $directory.$index[$i];
-		if (strlen($index[$i]) > CONF_MAX) $name = htmlentities(substr($index[$i], 0, CONF_MAX - 2))."..";
-		else $name = htmlentities($index[$i]);
-		if (is_dir($path)) printf(get_item(htmlentities($directory), $i, "file-directory.svg", $index, $name, htmlentities($path)."/", "?path="));
-		else printf(get_item(htmlentities($directory), $i, image(strtolower(strrchr($index[$i], "."))), $index, $name, htmlentities($path), ""));
+		if (strlen($index[$i]) > CONF_MAX) $name = substr($index[$i], 0, CONF_MAX - 2)."..";
+		else $name = $index[$i];
+		if (is_dir($path)) printf(get_item($directory, $i, "file-directory.svg", $index, $name, $path."/", "?path="));
+		else printf(get_item($directory, $i, image(strtolower(strrchr($index[$i], "."))), $index, $name, $path, ""));
 	}
 	if ($length < CONF_COL) for ($i = $length; $i < CONF_COL; $i++) printf("<td>\n</td>\n");
 	printf("</tr>\n");
 }
-else printf(get_item(htmlentities($directory), $i, "file-symlink-directory.svg", $index, "[Parent]", dirname($directory)."/", "?path=")."<td>\n</td>\n<td>\n</td>\n<td>\n</td>\n</tr>\n");
+else printf(get_item($directory, $i, "file-symlink-directory.svg", $index, "[Parent]", dirname($directory)."/", "?path=")."<td>\n</td>\n<td>\n</td>\n<td>\n</td>\n</tr>\n");
 
 ?>
 </table>
