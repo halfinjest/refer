b92ceeebe7b3769917f592eeb21416e015b0110f
Jacob Allen Utley <jacob@halfinjest.com>
2017-09-01 20:30:20 -0500

    Another commit.

--- type.php
+++ type.php
@@ -4,6 +4,13 @@ function type($extension)
 {
 	switch ($extension)
 	{
+		case ".css":
+		case ".htm":
+		case ".html":
+		case ".js":
+		case ".php":
+		case ".xhtml":
+			return "code";
 		case ".bmp":
 		case ".gif":
 		case ".jpg":
@@ -14,22 +21,12 @@ function type($extension)
 		case ".svg":
 		case ".xcf":
 			return "image";
-		case ".doc":
-		case ".docx":
-		case ".msg":
-		case ".pages":
-		case ".rtf":
-		case ".txt":
-		case ".wpd":
-		case ".wps":
-			return "text";
 		case ".m4a":
 		case ".mid":
 		case ".mp3":
 		case ".mpa":
 		case ".wav":
 		case ".wma":
-			return "media";
 		case ".avi":
 		case ".flv":
 		case ".mov":
@@ -40,13 +37,15 @@ function type($extension)
 			return "media";
 		case ".pdf":
 			return "pdf";
-		case ".css":
-		case ".htm":
-		case ".html":
-		case ".js":
-		case ".php":
-		case ".xhtml":
-			return "code";
+		case ".doc":
+		case ".docx":
+		case ".msg":
+		case ".pages":
+		case ".rtf":
+		case ".txt":
+		case ".wpd":
+		case ".wps":
+			return "text";
 		default:
 			return "other";
 	}
@@ -56,17 +55,17 @@ function image($extension)
 {
 	switch (type($extension))
 	{
+		case "other":
+			return "file.svg";
+		case "code":
+			return "file-code.svg";
 		case "image":
 		case "media":
 			return "file-media.svg";
-		case "text":
-			return "file-text.svg";
 		case "pdf":
 			return "file-pdf.svg";
-		case "code":
-			return "file-code.svg";
-		case "other":
-			return "file.svg";
+		case "text":
+			return "file-text.svg";
 	}
 }
 
