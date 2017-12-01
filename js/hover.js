fd8fee051c3c790ef235e567500d804f332cf95c
Jacob Allen Utley <jacob@halfinjest.com>
2017-12-01 12:56:31 -0600

    Syntax changes.

--- js/hover.js
+++ js/hover.js
@@ -1,15 +1,22 @@
 function hover(background, id, path, type)
 {
 	document.getElementById(id).style.background = background ? "rgba(219, 219, 219, 0.6)" : "transparent";
+
 	document.getElementById("path").innerHTML = path;
+
 	switch (type)
 	{
 		case "":
 			document.getElementById("preview").style.backgroundImage = "";
+
 			break;
+
 		case "image":
 			document.getElementById("preview").style.backgroundImage = "url('" + path + "')";
+
 			break;
-		default: break;
+
+		default:
+			break;
 	}
 }
--- js/listen.js
+++ js/listen.js
@@ -2,10 +2,13 @@ function listen()
 {
 	window.addEventListener("keydown", function(event)
 	{
-	     if (event.keyCode == 16) document.getElementById("preview").style.visibility = "visible";
+	     if (event.keyCode == 16)
+			document.getElementById("preview").style.visibility = "visible";
 	});
+
 	window.addEventListener("keyup", function(event)
 	{
-	     if (event.keyCode == 16) document.getElementById("preview").style.visibility = "hidden";
+	     if (event.keyCode == 16)
+			document.getElementById("preview").style.visibility = "hidden";
 	});
 }
